2e36d56a2c6c7eb2167f2cd6568e559cff05dd7d
Utkarsh Singh <83408377+utkarsh7370@users.noreply.github.com>
2023-07-29 14:50:17 +0530

    test commit

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import Nav from "@/components/Nav";
 export default function Home() {
   return (
     <>
+      <h1 className="flex justify-center">Hello world</h1>
       <h1 className="flex justify-center">Kodikas 2K23 - Home Page</h1>
     </>
   );
