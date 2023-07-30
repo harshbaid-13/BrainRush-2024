bda3ef6612916cb5c7f45b39ec237ff83021ae44
Pratik Agarwal <pratikkumar.agarwal123@gmail.com>
2023-07-30 23:37:50 +0530

    fixed al team bugs and changed url

--- app/api/team/all/route.js
+++ app/api/team/all/route.js
@@ -4,13 +4,11 @@ import Team from "@models/team";
 export async function GET() {
   try {
     await connectToDatabase();
-
-    const teams = await Team.find({}, "teamName");
-
-    const teamNames = teams.map((team) => team.teamName);
-
+    const teams = await Team.find();
     return NextResponse.json({
-      teamNames,
+      success: true,
+      message: "All registered teams",
+      data: teams,
     });
   } catch (error) {
     console.error("Error fetching team names:", error);
