a5fb77f412f7298975a3689c0ea15605ad3666f8
Nitu Kumari <78076309+U220053@users.noreply.github.com>
2023-08-01 02:44:47 +0530

    added remove member by leader,exit team by member

---
+++ app/api/team/[id]/remove/route.js
@@ -0,0 +1,63 @@
+import { NextResponse } from "next/server";
+import Team from "@models/team";
+import { connectToDatabase } from "@utils/db";
+
+export async function DELETE(request, { params }) {
+  try {
+    await connectToDatabase();
+    const userId = params.id;
+
+    const team = await Team.findOne({ leader: userId });
+    // Check if payment is done
+    if (team.payment) {
+      return NextResponse.json(
+        {
+          message:
+            "You cannot remove the team member after completing the payment",
+        },
+        { status: 403 }
+      );
+    }
+    const user = await Team.findByIdAndUpdate(
+      team._id,
+      { teamMember: null, teamMemberConfirmation: false },
+      { new: true }
+    );
+
+    return NextResponse.json({
+      success: true,
+      message: "Team member removed successfully",
+    });
+  } catch (error) {
+    console.error("Error removing team member:", error);
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
+
+// export async function DELETE(request, { params }) {
+//   try {
+//     await connectToDatabase();
+//     const userId = params.id;
+
+//     const team = await Team.findOne({ teamMember: userId });
+//     const user = await Team.findByIdAndUpdate(
+//       team._id,
+//       { teamMember: null, teamMemberConfirmation: false },
+//       { new: true }
+//     );
+
+//     return NextResponse.json({
+//       success: true,
+//       message: "Team member removed successfully",
+//     });
+//   } catch (error) {
+//     console.error("Error removing team member:", error);
+//     return NextResponse.json(
+//       { message: "Internal Server Error" },
+//       { status: 500 }
+//     );
+//   }
+//}
