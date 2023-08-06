121efec6c919b2e11e562c2747a6033a9294d218
Pratik Agarwal <pratikkumar.agarwal123@gmail.com>
2023-08-07 01:33:01 +0530

    integrated team api in frontend

--- app/teams/page.jsx
+++ app/teams/page.jsx
@@ -7,13 +7,89 @@ import "./page.css";
 
 const Teams = () => {
   const { data: session } = useSession();
+  const userId = session?.user?.id;
   const [qrData, setQrData] = useState();
   const getQr = async () => {
-    const response = await fetch(`/api/test/${session?.user?.id}`);
+    const response = await fetch(`/api/test/${userId}`);
     const data = await response.json();
     setQrData(data);
   };
-  const [creatTeam, setCreatTeam] = useState(false);
+  const [createTeam, setCreateTeam] = useState(false);
+
+  const [teamName, setTeamName] = useState("");
+  const [teamMemberEmail, setTeamMemberEmail] = useState("");
+  const [submit, setSubmit] = useState(false);
+
+  const [requests, setRequests] = useState([]);
+
+  const handleCreateTeamSubmit = async () => {
+    try {
+      await Promise.all([
+        fetch("/api/team/create", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ teamName, userId }),
+        }),
+        fetch("/api/team/confirm", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ teamMemberEmail, userId }),
+        }),
+      ]);
+      setSubmit(true);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleAcceptTeam = async (teamId) => {
+    try {
+      await fetch("/api/team/confirm", {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ userId, teamId }),
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleRejectTeam = async (teamId) => {
+    try {
+      await fetch("/api/team/confirm", {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ userId, teamId }),
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const getRequests = async () => {
+    const response = await fetch(`/api/team/confirm/${userId}`);
+    const data = await response.json();
+    setRequests(data.data);
+  };
+
+  const successSubmit = () => {
+    if (submit) {
+      return <page />;
+    }
+  };
+
+  useEffect(() => {
+    getRequests();
+  }, []);
+
   function teamForm() {
     return (
       <>
@@ -27,20 +103,11 @@ const Teams = () => {
               Got a technical issue? Want to send feedback about a beta feature?
               Need details about our Business plan? Let us know.
             </p>
-            <form action="#" class="space-y-8">
-              <div>
-                <label for="subject" class="block mb-2 text-sm font-medium ">
-                  Other's Email<span className="text-red text-2xl">*</span>
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                  placeholder="Other's Email"
-                  required
-                  //   value={email}
-                />
-              </div>
+            <form
+              action="#"
+              class="space-y-8"
+              onSubmit={handleCreateTeamSubmit}
+            >
               <div>
                 <label for="subject" class="block mb-2 text-sm font-medium ">
                   Team Name<span className="text-red text-2xl">*</span>
@@ -50,21 +117,32 @@ const Teams = () => {
                   id="email"
                   class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                   placeholder="Team Name"
-                  //   value={contact}
-                  //   onChange={(e) => {
-                  //     setContact(e.target.value);
-                  //   }}
                   required
+                  value={teamName}
+                  onChange={(e) => {
+                    setTeamName(e.target.value);
+                  }}
                 />
               </div>
-
+              <div>
+                <label for="subject" class="block mb-2 text-sm font-medium ">
+                  Team Member Email<span className="text-red text-2xl">*</span>
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                  placeholder="Team Member Email"
+                  required
+                  value={teamMemberEmail}
+                  onChange={(e) => {
+                    setTeamMemberEmail(e.target.value);
+                  }}
+                />
+              </div>
               <button
                 type="submit"
                 class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800"
-                // onClick={() => {
-                //   submitHandler;
-                //   setSubmit(true);
-                // }}
               >
                 <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
                   Submit Details
@@ -83,62 +161,65 @@ const Teams = () => {
       <section class="text-gray-600  px-2 body-font">
         <div class="container px-5 py-24 mx-auto flex flex-wrap">
           <div class="flex flex-wrap -m-4">
-            <div class="p-4 lg:w-1/2 md:w-full teamButton">
-              <div class="flex border-2 rounded-lg border-gray-200 border-opacity-50 p-8 sm:flex-row flex-col">
-                <div class="w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-8 h-8"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-                  </svg>
-                </div>
-                <div class="flex-grow">
-                  <h2 class="text-white text-6xl title-font font-2xl mb-3">
-                    Join Team
-                  </h2>
-                  <h1 class="leading-relaxed text-base">Team : Poogle</h1>
-                  <p class="leading-relaxed text-base mb-5">
-                    Other Member: Pratik Kumar Agarwal
-                  </p>
-                  <button
-                    type="submit"
-                    class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800"
-                    // onClick={() => {
-                    //   submitHandler;
-                    //   setSubmit(true);
-                    // }}
-                  >
-                    <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                      Cancel Request
-                    </span>
-                  </button>
-                  {/* <a class="mt-3 text-indigo-500 inline-flex me-2 items-center">
+            {requests &&
+              requests.map((request) => (
+                <div class="p-4 lg:w-1/2 md:w-full teamButton">
+                  <div class="flex border-2 rounded-lg border-gray-200 border-opacity-50 p-8 sm:flex-row flex-col">
+                    <div class="w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
+                      <svg
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        class="w-8 h-8"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+                      </svg>
+                    </div>
+                    <div class="flex-grow">
+                      <h2 class="text-white text-6xl title-font font-2xl mb-3">
+                        Join Team
+                      </h2>
+                      <h1 class="leading-relaxed text-base">
+                        Team Name : {request.team}
+                      </h1>
+                      <p class="leading-relaxed text-base mb-5">
+                        Team Leader: {request.teamLeader.name}
+                      </p>
+                      <button
+                        type="submit"
+                        class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800"
+                        onClick={() => {
+                          handleRejectTeam(request.team);
+                        }}
+                      >
+                        <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                          Cancel Request
+                        </span>
+                      </button>
+                      {/* <a class="mt-3 text-indigo-500 inline-flex me-2 items-center">
                     <Buttons title={"Cancel Request"} />
                   </a> */}
-                  <button
-                    type="submit"
-                    class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800"
-                    // onClick={() => {
-                    //   submitHandler;
-                    //   setSubmit(true);
-                    // }}
-                  >
-                    <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                      Confirm Request
-                    </span>
-                  </button>
-                  {/* <a class="mt-3 text-indigo-500 inline-flex ms-2 items-center">
+                      <button
+                        type="submit"
+                        class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800"
+                        onClick={() => {
+                          handleAcceptTeam(request.team);
+                        }}
+                      >
+                        <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                          Confirm Request
+                        </span>
+                      </button>
+                      {/* <a class="mt-3 text-indigo-500 inline-flex ms-2 items-center">
                     <Buttons title={"Confirm Request"} />
                   </a> */}
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
+              ))}
             <div class="p-4 lg:w-1/2 md:w-full teamButton">
               <div class="flex border-2 rounded-lg border-gray-200 border-opacity-50 p-8 sm:flex-row flex-col">
                 <div class="w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
@@ -167,7 +248,7 @@ const Teams = () => {
                     type="submit"
                     class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800"
                     onClick={() => {
-                      setCreatTeam(true);
+                      setCreateTeam(true);
                     }}
                   >
                     <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
@@ -175,7 +256,7 @@ const Teams = () => {
                     </span>
                   </button>
                   {/* <a class="mt-3 text-indigo-500 inline-flex items-center" onClick={() => {
-                                        setCreatTeam(true)
+                                        setCreateTeam(true)
                                     }}>
                                         <Buttons title={"Create Team"} />
                                     </a> */}
@@ -184,7 +265,7 @@ const Teams = () => {
             </div>
           </div>
         </div>
-        <div className="">{creatTeam ? teamForm() : ""}</div>
+        <div className="">{createTeam ? teamForm() : ""}</div>
       </section>
 
       {/* <h1 className="flex justify-center text-white">Kodikas 2K23 - Teams Page</h1>
