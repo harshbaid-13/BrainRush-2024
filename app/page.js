6559cd5a32021e6bc178c44efa76a02543032ebe
Utkarsh Singh <83408377+utkarsh7370@users.noreply.github.com>
2023-08-11 12:10:37 +0530

    normal margin added in heading

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
 // import Nav from "@/components/Nav";
 import Hero from "@components/HeroSection/Hero";
 import About from "@components/About/About";
-import Contact from "@/components/Contact/Contact";
-import Details from "@/components/Details/Details";
+// import Contact from "@/components/Contact/Contact";
+// import Details from "@/components/Details/Details";
 import FAQ from "@/components/F&Q/FAQ";
 import Footer from "@/components/Footer/Footer";
 // import Hero from "@/components/Hero";
@@ -10,8 +10,8 @@ import Footer from "@/components/Footer/Footer";
 // import Navbar from "@/components/Navbar";
 import Team from "@/components/Team/Team";
 import Venue from "@/components/Venue/Venue";
-import UserLanding from "@components/UserInterface/UserLanding";
-import Marquee from "@components/Marquee/Marquee";
+// import UserLanding from "@components/UserInterface/UserLanding";
+// import Marquee from "@components/Marquee/Marquee";
 // import Proccess from "@components/Details/Proccess";
 // import Details from "@/components/Details/Details";
 
