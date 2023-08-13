0e47e522ebf56a81768986d4777e2bfd76e24688
am-here <harshbaid.ind@gmail.com>
2023-08-13 19:32:24 +0530

    new file:   Reducers/features/profile.js 	new file:   Reducers/features/team.js 	modified:   Reducers/store.js 	modified:   app/api/user/[id]/route.js 	modified:   app/page.js 	modified:   app/profile/page.jsx 	modified:   components/Nav/Nav.jsx

--- Reducers/store.js
+++ Reducers/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "@Reducers/features/user.js";
+import userReducer from "@Reducers/features/user";
+import teamReducer from "@Reducers/features/team";
+import profileReducer from "@Reducers/features/profile";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
+    team: teamReducer,
+    profile: profileReducer,
   },
 });
--- app/page.js
+++ app/page.js
@@ -11,6 +11,12 @@ import Footer from "@/components/Footer/Footer";
 // import Navbar from "@/components/Navbar";
 import Team from "@/components/Team/Team";
 import Venue from "@/components/Venue/Venue";
+import { useDispatch, useSelector } from "react-redux";
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
+import { setUser } from "@Reducers/features/user";
+import { setTeam } from "@Reducers/features/team";
+import { setProfile } from "@Reducers/features/profile";
 
 // import UserLanding from "@components/UserInterface/UserLanding";
 // import Marquee from "@components/Marquee/Marquee";
@@ -18,8 +24,39 @@ import Venue from "@/components/Venue/Venue";
 // import Details from "@/components/Details/Details";
 
 export default function Home() {
+  const user = useSelector((state) => state.user.user);
+  const team = useSelector((state) => state.team.team);
+  const profile = useSelector((state) => state.profile.profile);
+  // const isProfileCompleted = useSelector(
+  //   (state) => state.profile.isProfileCompleted
+  // );
+  const dispatch = useDispatch();
+  const { data: session } = useSession();
+
+  const setUserdata = () => {
+    dispatch(setUser(session?.user));
+  };
+  const getTeamDetails = async () => {
+    const res = await fetch(`/api/team/${user?.id}/displayoneteam`);
+    const { data } = await res.json();
+    dispatch(setTeam(data));
+  };
+  const getProfileDetails = async () => {
+    const res = await fetch(`/api/user/${user?.id}`);
+    const { data } = await res.json();
+    dispatch(setProfile(data));
+  };
+  useEffect(() => {
+    setUserdata();
+    // getTeamDetails();
+    getProfileDetails();
+  }, [session]);
+
   return (
     <>
+      {console.log("hello reducer1: " + profile?.id)}
+      {/* {console.log("hello reducer2: " + isProfileCompleted)} */}
+      {console.log("hello reducer3: " + profile?.year)}
       <Hero />
       <About />
       {/* <Details /> */}
