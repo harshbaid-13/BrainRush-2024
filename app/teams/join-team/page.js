8f92af792b60698a8ccee328f662fbd40b3c4873
Utkarsh Singh <83408377+utkarsh7370@users.noreply.github.com>
2023-08-14 01:47:39 +0530

    Join team Cancel team ui

--- app/teams/join-team/page.js
+++ app/teams/join-team/page.js
@@ -68,12 +68,12 @@ function page() {
         ) : (
           <>
             <section className="text-gray-600  px-2 body-font">
-              <div className="container px-5 py-24 mx-auto flex flex-wrap">
+              <div className="container px-5 mx-auto flex flex-wrap">
                 <div className="flex flex-wrap -m-4">
                   {requests &&
                     requests.map((request) => (
-                      <div className="p-4 lg:w-1/2 md:w-full teamButton">
-                        <div className="flex border-2 rounded-lg border-gray-200 border-opacity-50 p-8 sm:flex-row flex-col">
+                      <div className="p-4 w-2/3 teamButton mx-auto">
+                        <div className="flex border-2 rounded-lg border-gray-200 border-opacity-50 p-8 sm:flex-row flex-row">
                           <div className="w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
                             <svg
                               fill="none"
@@ -87,54 +87,53 @@ function page() {
                               <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
                             </svg>
                           </div>
-                          <div className="flex-grow">
-                            <h2 className="text-white text-6xl title-font font-2xl mb-3">
-                              <span className={preahvihear.className}>
-                                Join Team
-                              </span>
-                            </h2>
-                            <h1 className="leading-relaxed text-base">
-                              <span className={preahvihear.className}>
-                                Team Name : {request.team.teamName}
-                              </span>
-                              {/* Team Name : Poogle */}
-                            </h1>
-                            <p className="leading-relaxed text-base mb-5">
-                              <span className={preahvihear.className}>
-                                Team Leader: {request.teamLeader.name}
-                              </span>
-                              {/* Team Leader: Pratik Kumar Agarwal */}
-                            </p>
-                            <button
-                              type="submit"
-                              onClick={() => {
-                                handleRejectTeam(request.team);
-                              }}
-                              className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-btnColorDark to-btnColor hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800"
-                            >
-                              <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                          <div className="flex flex-row w-full items-center justify-between">
+                            <div>
+                              <h1 className="leading-relaxed text-base">
                                 <span className={preahvihear.className}>
-                                  Confirm Request
+                                  Team Name : {request.team.teamName}
                                 </span>
-                              </span>
-                            </button>
+                                {/* Team Name : Poogle */}
+                              </h1>
+                              <p className="leading-relaxed text-base mb-5">
+                                <span className={preahvihear.className}>
+                                  Team Leader: {request.teamLeader.name}
+                                </span>
+                                {/* Team Leader: Pratik Kumar Agarwal */}
+                              </p>
+                            </div>
 
-                            {/* <a className="mt-3 text-indigo-500 inline-flex me-2 items-center">
+                            <div>
+                              {/* <a className="mt-3 text-indigo-500 inline-flex me-2 items-center">
                     <Buttons title={"Cancel Request"} />
                   </a> */}
-                            <button
-                              type="submit"
-                              onClick={() => {
-                                handleAcceptTeam(request.team);
-                              }}
-                              className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-btnColorDark to-btnColor hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800"
-                            >
-                              <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                                <span className={preahvihear.className}>
-                                  Confirm Request
+                              <button
+                                type="submit"
+                                onClick={() => {
+                                  handleAcceptTeam(request.team);
+                                }}
+                                className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-btnColorDark to-btnColor hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800"
+                              >
+                                <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white text-gray-900 hover:text-gray-50  rounded-md group-hover:bg-opacity-0">
+                                  <span className={preahvihear.className}>
+                                    Confirm
+                                  </span>
                                 </span>
-                              </span>
-                            </button>
+                              </button>
+                              <button
+                                type="submit"
+                                onClick={() => {
+                                  handleRejectTeam(request.team);
+                                }}
+                                className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-btnColorDark to-btnColor hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800"
+                              >
+                                <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white text-gray-900 hover:text-gray-50  rounded-md group-hover:bg-opacity-0">
+                                  <span className={preahvihear.className}>
+                                    Ignore
+                                  </span>
+                                </span>
+                              </button>
+                            </div>
 
                             {/* <a className="mt-3 text-indigo-500 inline-flex ms-2 items-center">
                     <Buttons title={"Confirm Request"} />
