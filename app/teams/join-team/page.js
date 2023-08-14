ddab9fed984e4fcb2487554e4e44dde6e218c7b1
Utkarsh Singh <83408377+utkarsh7370@users.noreply.github.com>
2023-08-14 17:37:18 +0530

    Join Team UI Responsive

--- app/teams/join-team/page.js
+++ app/teams/join-team/page.js
@@ -62,30 +62,30 @@ function page() {
         ) : (
           <>
             <section className="text-gray-600  px-2 body-font">
-              <div className="container px-5 mx-auto flex flex-wrap">
-                <div className="flex flex-wrap -m-4">
+              <div className="containe mx-auto">
+                <div className="flex flex-col -m- w-full items-center justify-center">
                   {requests &&
                     requests.map((request) => (
-                      <div className="p-4 w-2/3 teamButton mx-auto">
-                        <div className="flex border-2 rounded-lg border-gray-200 border-opacity-50 p-8 sm:flex-row flex-row">
-                          <div className="w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
+                      <div className="p-4 sm:w-full md:w-4/5 lg:w-2/3 teamButton mx-auto">
+                        <div className="flex border-2 rounded-lg border-gray-200 border-opacity-50 sm:p-3 lg:p-8 sm:flex-row flex-row">
+                          <div className="w-16 h-16 sm:hidden lg:inline sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
                             <svg
                               fill="none"
                               stroke="currentColor"
                               stroke-linecap="round"
                               stroke-linejoin="round"
                               strokeWidth="2"
-                              className="w-8 h-8"
+                              className="w-8 h-8 sm:hidden lg:inline"
                               viewBox="0 0 24 24"
                             >
                               <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
                             </svg>
                           </div>
-                          <div className="flex flex-row w-full items-center justify-between">
+                          <div className="flex flex-row flex-wrap w-full items-center sm:justify-center md:justify-between">
                             <div>
                               <h1 className="leading-relaxed text-base">
                                 <span className={preahvihear.className}>
-                                  Team Name : {request.team.teamName}
+                                  Team Name : {request?.team?.teamName}
                                 </span>
                                 {/* Team Name : Poogle */}
                               </h1>
@@ -97,7 +97,7 @@ function page() {
                               </p>
                             </div>
 
-                            <div>
+                            <div className="flex flex:wrap items-center justify-end">
                               {/* <a className="mt-3 text-indigo-500 inline-flex me-2 items-center">
                     <Buttons title={"Cancel Request"} />
                   </a> */}
