72a0dd09d1d2cb343a7c076c6643fac334324123
Ishika Ghosh <idofishika321@gmail.com>
2023-08-16 01:41:55 +0530

    callback url added in sign in

--- components/Nav/Nav.jsx
+++ components/Nav/Nav.jsx
@@ -209,7 +209,7 @@ function Navbar() {
                     <button
                       className="relative inline-flex items-center justify-center p-0.5 pr-2 mb-0  overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-btnColorDark to-btnColor group-hover:from-btnColorDark group-hover:to-btnColor hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800"
                       key={provider.name}
-                      onClick={() => signIn(provider.id)}
+                      onClick={() => signIn(provider.id, { callbackUrl: "/" })}
                     >
                       <span className="relative px-3 py-2 transition-all ease-in duration-75 bg-white text-gray-700 rounded-md hover:text-gray-100 group-hover:bg-opacity-0">
                         <span className={preahvihear.className}>Sign In</span>
