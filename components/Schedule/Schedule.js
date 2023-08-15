33b4c4bfdf3764ebdcdc27f2fcba5f806b3721d6
Utkarsh Singh <83408377+utkarsh7370@users.noreply.github.com>
2023-08-16 00:38:32 +0530

    class to className

--- components/Schedule/Schedule.js
+++ components/Schedule/Schedule.js
@@ -12,15 +12,15 @@ function Schedule() {
   return (
     <>
       <Heading title="Our Schedule" />
-      <section class=" dark:text-headerText">
-        <div class="container max-w-5xl px-4 pt-3 pb-12 mx-auto">
-          <div class="grid gap-4 mx-4 sm:grid-cols-12">
-            <div class="col-span-12 sm:col-span-3">
-              <div class="text-center sm:text-left mb-14 mr-3 before:block before:w-24 before:h-3 before:mb-5 before:rounded-md before:mx-auto sm:before:mx-0 before:dark:bg-violet-400">
-                <h3 class="text-4xl font-semibold text-headerText">
+      <section className=" dark:text-headerText">
+        <div className="container max-w-5xl px-4 pt-3 pb-12 mx-auto">
+          <div className="grid gap-4 mx-4 sm:grid-cols-12">
+            <div className="col-span-12 sm:col-span-3">
+              <div className="text-center sm:text-left mb-14 mr-3 before:block before:w-24 before:h-3 before:mb-5 before:rounded-md before:mx-auto sm:before:mx-0 before:dark:bg-violet-400">
+                <h3 className="text-4xl font-semibold text-headerText">
                   <span className={preahvihear.className}>Kodikas-2K23</span>
                 </h3>
-                <span class="text-sm font-bold tracking-wider uppercase dark:text-gray-400">
+                <span className="text-sm font-bold tracking-wider uppercase dark:text-gray-400">
                   <span className={preahvihear.className}>
                     {/* Kodikas-2K23 */}
                     Innovate your code game
@@ -35,22 +35,22 @@ function Schedule() {
                 />
               </div>
             </div>
-            <div class="relative col-span-12 px-4 space-y-6 sm:col-span-9">
-              <div class="col-span-12 space-y-12 relative px-4 sm:col-span-8 sm:space-y-8 sm:before:absolute sm:before:top-2 sm:before:bottom-0 sm:before:w-0.5 sm:before:-left-3 before:dark:bg-headerText">
-                <div class="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:dark:bg-subHeaderText">
-                  <h3 class="text-3xl font-bold tracking-wide text-subHeaderText">
+            <div className="relative col-span-12 px-4 space-y-6 sm:col-span-9">
+              <div className="col-span-12 space-y-12 relative px-4 sm:col-span-8 sm:space-y-8 sm:before:absolute sm:before:top-2 sm:before:bottom-0 sm:before:w-0.5 sm:before:-left-3 before:dark:bg-headerText">
+                <div className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:dark:bg-subHeaderText">
+                  <h3 className="text-3xl font-bold tracking-wide text-subHeaderText">
                     <span className={preahvihear.className}>
                       {/* Kodikas-2K23 */}
                       KodeTeaser
                     </span>
                   </h3>
-                  <time class="text-xs tracking-wide uppercase dark:text-gray-400">
+                  <time className="text-xs tracking-wide uppercase dark:text-gray-400">
                     <span className={preahvihear.className}>
                       {/* Kodikas-2K23 */}
                       24-Aug 11:00 Am
                     </span>
                   </time>
-                  <p class="mt-3">
+                  <p className="mt-3">
                     <span className={preahvihear.className}>
                       {/* Kodikas-2K23 */}
                       • Teams will compete in a thrilling coding knowledge
@@ -60,20 +60,20 @@ function Schedule() {
                     </span>
                   </p>
                 </div>
-                <div class="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:dark:bg-subHeaderText">
-                  <h3 class="text-3xl font-bold tracking-wide text-subHeaderText">
+                <div className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:dark:bg-subHeaderText">
+                  <h3 className="text-3xl font-bold tracking-wide text-subHeaderText">
                     <span className={preahvihear.className}>
                       {/* Kodikas-2K23 */}
                       KoderRank
                     </span>
                   </h3>
-                  <time class="text-xs tracking-wide uppercase dark:text-gray-400">
+                  <time className="text-xs tracking-wide uppercase dark:text-gray-400">
                     <span className={preahvihear.className}>
                       {/* Kodikas-2K23 */}
                       24-Aug 12:30 Pm
                     </span>
                   </time>
-                  <p class="mt-3">
+                  <p className="mt-3">
                     <span className={preahvihear.className}>
                       {/* Kodikas-2K23 */}
                       • Teams will tackle coding challenges using their favorite
@@ -83,20 +83,20 @@ function Schedule() {
                     </span>
                   </p>
                 </div>
-                <div class="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:dark:bg-subHeaderText">
-                  <h3 class="text-3xl font-semibold tracking-wide text-subHeaderText">
+                <div className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:dark:bg-subHeaderText">
+                  <h3 className="text-3xl font-semibold tracking-wide text-subHeaderText">
                     <span className={preahvihear.className}>
                       {/* Kodikas-2K23 */}
                       Kodikas Premier League ( KPL )
                     </span>
                   </h3>
-                  <time class="text-xs tracking-wide uppercase dark:text-gray-400">
+                  <time className="text-xs tracking-wide uppercase dark:text-gray-400">
                     <span className={preahvihear.className}>
                       {/* Kodikas-2K23 */}
                       24-Aug 02:30 Pm
                     </span>
                   </time>
-                  <p class="mt-3">
+                  <p className="mt-3">
                     <span className={preahvihear.className}>
                       {/* Kodikas-2K23 */}
                       •Teams will go head-to-head in 1 vs 1 battles, aiming to
