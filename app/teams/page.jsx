78dee20becac2d3acb044e5bddb2eca2912ce071
am-here <harshbaid.ind@gmail.com>
2023-08-15 21:49:27 +0530

    ui added

--- app/teams/page.jsx
+++ app/teams/page.jsx
@@ -23,6 +23,7 @@ const Teams = () => {
   );
   const user = useSelector((state) => state.user.user);
   const [qrData, setQrData] = useState();
+  const [teamMemberEmail, setTeamMemberEmail] = useState("")
   //team qr not working
   const getQr = async () => {
     setLoading(true);
@@ -71,7 +72,7 @@ const Teams = () => {
       console.log(error);
     }
   };
-  const handleRemove = async () => {
+  const handleRemoveMember = async () => {
     try {
       setLoading(true);
       const res = await fetch(`/api/new/${team?._id}`, {
@@ -109,6 +110,34 @@ const Teams = () => {
       console.log(error);
     }
   };
+  const handleSendRequest = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("/api/new/confirm", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ teamMemberEmail }),
+      });
+      const data = await res.json();
+      if (data.success) {
+        dispatch(
+          setTeamRequest(
+            data.confirmationRequest ? data.confirmationRequest : null
+          )
+        );
+        setTeamMemberEmail("");
+      } else {
+        alert(confirmation.message);
+      }
+      setLoading(false);
+      // router.push("/teams");
+    } catch (err) {
+      console.log(err);
+    }
+  }
   useEffect(() => {
     setLoading(true);
     setTimeout(() => {
@@ -142,75 +171,77 @@ const Teams = () => {
                         </svg>
                       </div>
                       <div className="flex-grow">
-                        <h2 className="text-headerText text-4xl title-font font-2xl mb-3">
+                        <h2 className="text-headerText text-4xl title-font font-2xl mb-6">
                           <span className={preahvihear.className}>My Team</span>
                         </h2>
-                        <h1 className="leading-relaxed text-base text-gray-200 mb-5 ">
+                        <h1 className="leading-relaxed text-base text-gray-200 mb-3 ">
                           <span className={preahvihear.className}>
                             {" "}
                             Team Name : {team?.teamName}
                           </span>
                         </h1>
-                        <p className="leading-relaxed text-base text-gray-200 mb-5">
+                        <p className="leading-relaxed text-base text-gray-200 mb-3">
                           <span className={preahvihear.className}>
                             Team Leader: {team?.leader?.name}
                           </span>
                         </p>
-                        <p className="leading-relaxed text-base text-gray-200 mb-5">
+                        <p className="leading-relaxed text-base text-gray-200 mb-3">
                           <span className={preahvihear.className}>
                             Payment Status:{" "}
                             {team?.payment ? "Paid" : "Not Paid"}
                           </span>
                         </p>
                         {team?.teamMemberConfirmation ? (
-                          <p>
+                          <p className="leading-relaxed text-base text-gray-200 mb-3">
                             <span className={preahvihear.className}>
                               Team Member: {team?.teamMember.name}
                             </span>{" "}
                           </p>
                         ) : (
-                          <p>
+                          <p className="leading-relaxed text-base text-gray-200 mb-3">
                             <span className={preahvihear.className}>
                               Team Member: Not Joined
                             </span>
                           </p>
                         )}
-                        <form className="space-y-8 mt-5">
-                          <div className="w-full flex items-center">
-                            <label
-                              htmlFor="email"
-                              className="block text-md mr-2 text-gray-700 font-medium"
-                            >
-                              <span className={preahvihear.className}>
-                                Add Team Member
-                                <span className="text-red text-md">: </span>
-                              </span>{" "}
-                            </label>
-                            <div>
-                              <input
-                                type="text"
-                                id="email"
-                                className="shadow-sm bg-inputBgColor border-gray-300 text-gray-900 text-md rounded-lg focus:ring-primary-500 focus:border-gray-50 block  p-1"
-                                placeholder="Team Member Email"
-                                required
-                                // value={name}
-                                // onChange={(e) => {
-                                //   setName(e.target.value);
-                                // }}
-                              />
-                            </div>
-                            <button
-                              onClick={handleRemoveRequest}
-                              className="relative text-center inline-flex items-center justify-center p-0.5 ml-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-btnColorDark to-btnColor "
-                            >
-                              <span className="relative px-2.5 py-1.5 transition-all ease-in bg-white text-gray-700 duration-75 rounded-md group-hover:bg-opacity-0 group-hover:text-white">
+                        {!team?.teamMemberConfirmation && !sentRequestFromTheTeam &&
+                          <form className="space-y-8 " onSubmit={handleSendRequest}>
+                            <div className="w-full flex items-center">
+                              <label
+                                htmlFor="email"
+                                className="block text-md mr-2 text-gray-700 font-medium"
+                              >
                                 <span className={preahvihear.className}>
-                                  Send
+                                  Add Team Member:
+                                  <span className="text-red text-md"> </span>
+                                </span>{" "}
+                              </label>
+                              <div>
+                                <input
+                                  type="text"
+                                  id="email"
+                                  className="shadow-sm bg-inputBgColor border-gray-300 text-gray-900 text-md rounded-lg focus:ring-primary-500 focus:border-gray-50 block  p-1"
+                                  placeholder="Team Member Email"
+                                  required
+                                  value={teamMemberEmail}
+                                  onChange={(e) => {
+                                    setTeamMemberEmail(e.target.value);
+                                  }}
+                                />
+                              </div>
+                              <button
+                                type="submit"
+                                className="relative text-center inline-flex items-center justify-center p-0.5 ml-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-btnColorDark to-btnColor "
+                              >
+                                <span className="relative px-2.5 py-1.5 transition-all ease-in bg-white text-gray-700 duration-75 rounded-md group-hover:bg-opacity-0 group-hover:text-white">
+                                  <span className={preahvihear.className}>
+                                    Send
+                                  </span>
                                 </span>
-                              </span>
-                            </button>
-                          </div>
-                        </form>
+                              </button>
+                            </div>
+                          </form>}
+
                         {!team?.teamMemberConfirmation &&
                           sentRequestFromTheTeam && (
                             <h1 className={preahvihear.className}>
@@ -223,7 +254,9 @@ const Teams = () => {
                       </div>
                     </div>
                     {/* {console.log(qrData)} */}
+
                     <div className="flex flex-row justify-center items-center p-5">
+                      {/* qr code ui */}
                       {qrData && (
                         <img
                           className="mb-5 n"
@@ -233,7 +266,7 @@ const Teams = () => {
                           alt="qr"
                         />
                       )}
-
+                      {/* team code */}
                       {team?.teamMemberConfirmation && (
                         <button
                           type="submit"
@@ -247,11 +280,12 @@ const Teams = () => {
                           </span>
                         </button>
                       )}
+                      {/* kick member */}
                       {user?.id === team?.leader?._id &&
                         !team.payment &&
                         team?.teamMemberConfirmation && (
                           <button
-                            onClick={handleRemove}
+                            onClick={handleRemoveMember}
                             className="relative mt-5 text-center inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-btnColorDark to-btnColor hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800"
                           >
                             <span className="relative px-5 py-2.5 transition-all ease-in bg-white text-gray-700 duration-75 rounded-md group-hover:bg-opacity-0 group-hover:text-white">
@@ -261,6 +295,7 @@ const Teams = () => {
                             </span>
                           </button>
                         )}
+                      {/* remove request */}
                       {!team?.teamMemberConfirmation &&
                         sentRequestFromTheTeam && (
                           <button
@@ -274,6 +309,7 @@ const Teams = () => {
                             </span>
                           </button>
                         )}
+                      {/* delete team */}
                       {!team?.payment ? (
                         user?.id === team?.leader?._id ? (
                           <button
@@ -330,7 +366,7 @@ const Teams = () => {
                       <div className="flex-grow">
                         <h2
                           className="text-headerText text-4xl title-font font-2xl mb-3"
-                          // style={{ color: "#6f7bd9 !important" }}
+                        // style={{ color: "#6f7bd9 !important" }}
                         >
                           <span className={preahvihear.className}>
                             Join Team
@@ -414,7 +450,8 @@ const Teams = () => {
             </section>
           )}
         </>
-      )}
+      )
+      }
     </>
   );
 };
