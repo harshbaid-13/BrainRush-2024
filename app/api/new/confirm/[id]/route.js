4878e7913886a1b8de66308e1e6759d31bcab75c
Pratik Agarwal <pratikkumar.agarwal123@gmail.com>
2023-08-15 17:05:54 +0530

    api changes

---
+++ app/api/new/confirm/[id]/route.js
@@ -0,0 +1,138 @@
+import Team from "@models/team";
+import User from "@models/user";
+import { connectToDatabase } from "@utils/db";
+import ConfirmationRequest from "@models/confirmationRequest";
+import { NextResponse } from "next/server";
+import mongoose from "mongoose";
+import sendConfirmationEmail from "@utils/sendConfirmationEmail";
+
+// Delete Invite Request
+export async function PATCH(req, { params }) {
+  try {
+    await connectToDatabase();
+    const email = req.headers.get("Authorization");
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      return NextResponse.json({ success: false, message: "User not found" });
+    }
+    const userId = user._id;
+    const requestId = params.id;
+    if (!mongoose.Types.ObjectId.isValid(userId)) {
+      return NextResponse.json(
+        { message: "Not a valid user" },
+        { status: 404 }
+      );
+    }
+    let sentRequest = await ConfirmationRequest.findOne({
+      $and: [{ _id: requestId }, { teamLeader: userId }],
+    });
+
+    console.log({ sentRequest });
+
+    if (!sentRequest) {
+      return NextResponse.json({
+        success: false,
+        status: 404,
+        message: "No request found",
+      });
+    }
+
+    await sentRequest.deleteOne();
+
+    return NextResponse.json({
+      success: true,
+      status: 200,
+      message: "Request Deleted Successfully",
+    });
+  } catch (error) {
+    console.error("Error ", error);
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
+
+// Accept Invite Request
+export async function PUT(req, { params }) {
+  try {
+    await connectToDatabase();
+    const email = req.headers.get("Authorization");
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      return NextResponse.json({ success: false, message: "User not found" });
+    }
+    const userId = user._id;
+    const id = params.id;
+    const confirmationRequest = await ConfirmationRequest.findById(id);
+
+    const userTeamExist = await Team.findOne({ leader: userId });
+
+    if (userTeamExist) {
+      return NextResponse.json({
+        success: false,
+        status: 400,
+        message: "You have a team already",
+      });
+    }
+
+    const team = await Team.findByIdAndUpdate(
+      confirmationRequest?.team,
+      { teamMember: userId, teamMemberConfirmation: true },
+      { new: true }
+    )
+      .populate("leader")
+      .populate("teamMember");
+
+    await confirmationRequest.deleteOne();
+
+    return NextResponse.json({
+      success: true,
+      status: 200,
+      message: "Team Request Accepted Successfully",
+      data: team,
+    });
+  } catch (error) {
+    console.error("Error ", error);
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
+
+// Reject Invite Request
+export async function DELETE(req, { params }) {
+  try {
+    await connectToDatabase();
+    const email = req.headers.get("Authorization");
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      return NextResponse.json({ success: false, message: "User not found" });
+    }
+    const id = params.id;
+    const confirmationRequest = await ConfirmationRequest.findById(id)
+      .populate("teamLeader")
+      .populate("team");
+    sendConfirmationEmail(
+      confirmationRequest?.teamLeader,
+      confirmationRequest.team,
+      confirmationRequest.teamLeader.email,
+      { event: 1 }
+    );
+    await confirmationRequest.deleteOne();
+    //need to notify the leader that other member has rejected the request
+
+    return NextResponse.json({
+      success: true,
+      status: 200,
+      message: "Request Deleted Successfully",
+    });
+  } catch (error) {
+    console.error("Error ", error);
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
---
+++ app/api/new/confirm/route.js
@@ -0,0 +1,105 @@
+import ConfirmationRequest from "@models/confirmationRequest";
+import Team from "@models/team";
+import User from "@models/user";
+import { connectToDatabase } from "@utils/db";
+import sendConfirmationEmail from "@utils/sendConfirmationEmail";
+import mongoose from "mongoose";
+
+const { NextResponse } = require("next/server");
+
+// Send Invite Request
+export async function POST(req) {
+  try {
+    await connectToDatabase();
+    const email = req.headers.get("Authorization");
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      return NextResponse.json({ success: false, message: "User not found" });
+    }
+    const { teamMemberEmail } = await req.json();
+    if (teamMemberEmail === user.email) {
+      return NextResponse.json(
+        { success: false, message: "You can't add yourself as a member" },
+        { status: 400 }
+      );
+    }
+    const teamLeaderId = user._id;
+    const requestSentAlready = await ConfirmationRequest.findOne({
+      teamLeader: teamLeaderId,
+    });
+    if (requestSentAlready) {
+      return NextResponse.json({
+        success: false,
+        status: 400,
+        message: "Delete Previous Request First to Send New Request",
+      });
+    }
+    const team = await Team.findOne({ leader: teamLeaderId });
+    if (team.teamMemberConfirmation) {
+      return NextResponse.json({
+        success: false,
+        status: 400,
+        message: "Team Full",
+      });
+    }
+
+    sendConfirmationEmail(user, team, teamMemberEmail, { event: 0 });
+
+    const confirmationRequest = await ConfirmationRequest.create({
+      team,
+      teamLeader: user,
+      teamMemberEmail,
+    });
+
+    return NextResponse.json({
+      success: true,
+      status: 200,
+      message: "Email Sent Successfully",
+      data: confirmationRequest,
+    });
+  } catch (error) {
+    console.error("Error ", error);
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
+
+// Get All LoggedIn User Requests
+export async function GET(req) {
+  try {
+    await connectToDatabase();
+    const email = req.headers.get("Authorization");
+    console.log({ email });
+    const user = await User.findOne({ email: email });
+    console.log({ user });
+    if (!user) {
+      return NextResponse.json({ success: false, message: "User not found" });
+    }
+    const teamLeaderId = user._id;
+    if (!mongoose.Types.ObjectId.isValid(teamLeaderId)) {
+      return NextResponse.json(
+        { message: "Not a valid user" },
+        { status: 404 }
+      );
+    }
+    // const loggedInUser = await User.findById(userId);
+    const totalRequests = await ConfirmationRequest.find({
+      teamMemberEmail: user.email,
+    }).populate(["team", "teamLeader"]);
+
+    return NextResponse.json({
+      success: true,
+      status: 200,
+      message: "Requests Retrieved Successfully",
+      data: totalRequests,
+    });
+  } catch (error) {
+    console.error("Error ", error);
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
